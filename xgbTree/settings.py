152c29404ec0b2b93ca0f9eb8da406f6f5254d37
s.sokhansanj <sepehrsokhan78@gmail.com>
2026-09-20 18:13:17 +0330

    database changed to postgress

--- xgbTree/settings.py
+++ xgbTree/settings.py
@@ -54,10 +54,21 @@ WSGI_APPLICATION = 'xgbTree.wsgi.application'
 
 # Database
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
 }
 
